edffdec5ae91a5f3a15cc54bd6b9dba6c32f55a1
huangjinchao <grozta@163.com>
2023-10-24 11:17:04 +0800

    调整配置文件，增加预训练模型权重位置

--- more-scenarios/medical/unimatch_3D.py
+++ more-scenarios/medical/unimatch_3D.py
@@ -1,7 +1,7 @@
 import argparse
-import logging
 import os
 import pprint
+import shutil
 
 import torch
 from torch import nn
@@ -13,51 +13,42 @@ from torch.utils.tensorboard import SummaryWriter
 import yaml
 
 from dataset.dataset_3D import Dataset_3D
-from model.unet import UNet
+from model.unet_3D import UNet_3D
 from util.classes import CLASSES
-from util.utils import AverageMeter, count_params, init_log, DiceLoss
-from util.dist_helper import setup_distributed
+from util.utils import AverageMeter, count_params, Logger, DiceLoss
+from util.tools import *
 
 
 parser = argparse.ArgumentParser(description='Revisiting Weak-to-Strong Consistency in Semi-Supervised Semantic Segmentation')
-parser.add_argument('--config', type=str, required=True)
-parser.add_argument('--local_rank', default=0, type=int)
-parser.add_argument('--port', default=None, type=int)
-
+parser.add_argument('--config', type=str,default="configs/flare22.yaml")
+parser.add_argument('--restart_train', required=False, default=True, action="store_true",)
 
 def main():
     args = parser.parse_args()
+    cfg = load_yaml(args.config)
+    cfg["restart_train"]= args.restart_train
+    output_dir = os.path.abspath(join(cfg["output_dir_root"],cfg["project_name"],cfg["train_name"]))
+    if args.restart_train and isdir(output_dir):
+        shutil.rmtree(output_dir)
+    maybe_mkdir_p(output_dir)
 
-    cfg = yaml.load(open(args.config, "r"), Loader=yaml.Loader)
-
-    logger = init_log('global', logging.INFO)
-    logger.propagate = 0
-
-    rank, world_size = setup_distributed(port=args.port)
-
-    if rank == 0:
-        all_args = {**cfg, **vars(args), 'ngpus': world_size}
-        logger.info('{}\n'.format(pprint.pformat(all_args)))
-        
-        writer = SummaryWriter(cfg["save_path"])
-        
-        os.makedirs(cfg["save_path"], exist_ok=True)
+    log_dir = join(output_dir,"log")
+    maybe_mkdir_p(log_dir)
+    logger = Logger(join(log_dir,"log.txt")).logger
+    logger.info('{}\n'.format(pprint.pformat(cfg)))
     
+    writer = SummaryWriter(log_dir=log_dir)
+
     cudnn.enabled = True
     cudnn.benchmark = True
 
-    model = UNet(in_chns=1, class_num=cfg['nclass'])    
-    if rank == 0:
-        logger.info('Total params: {:.1f}M\n'.format(count_params(model)))
-        
+    model = UNet_3D(in_chns=1, class_num=cfg['nclass'],feature_chns = cfg['feature_chns'],dropout=cfg['dropout'])
+  
+    logger.info('Total params: {:.1f}M\n'.format(count_params(model)))
+
     optimizer = SGD(model.parameters(), cfg['lr'], momentum=0.9, weight_decay=0.0001)
 
-    local_rank = int(os.environ["LOCAL_RANK"])
-    model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
     model.cuda()
-
-    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], broadcast_buffers=False,
-                                                      output_device=local_rank, find_unused_parameters=False)
 
     criterion_ce = nn.CrossEntropyLoss()
     criterion_dice = DiceLoss(n_classes=cfg['nclass'])
@@ -66,18 +57,14 @@ def main():
     trainset_l = Dataset_3D('train_l',cfg, nsample=len(trainset_u.ids))
     valset = Dataset_3D('val',cfg)
 
-    trainsampler_l = torch.utils.data.distributed.DistributedSampler(trainset_l)
     trainloader_l = DataLoader(trainset_l, batch_size=cfg['batch_size'],
-                               pin_memory=True, num_workers=1, drop_last=True, sampler=trainsampler_l)
-    trainsampler_u = torch.utils.data.distributed.DistributedSampler(trainset_u)
+                               pin_memory=True, num_workers=cfg['num_workers'], drop_last=True)
     trainloader_u = DataLoader(trainset_u, batch_size=cfg['batch_size'],
-                               pin_memory=True, num_workers=1, drop_last=True, sampler=trainsampler_u)
-    trainsampler_u_mix = torch.utils.data.distributed.DistributedSampler(trainset_u)
+                               pin_memory=True, num_workers=cfg['num_workers'], drop_last=True)
     trainloader_u_mix = DataLoader(trainset_u, batch_size=cfg['batch_size'],
-                                   pin_memory=True, num_workers=1, drop_last=True, sampler=trainsampler_u_mix)
-    valsampler = torch.utils.data.distributed.DistributedSampler(valset)
-    valloader = DataLoader(valset, batch_size=1, pin_memory=True, num_workers=1,
-                           drop_last=False, sampler=valsampler)
+                                   pin_memory=True, num_workers=cfg['num_workers'], drop_last=True)
+    valloader = DataLoader(valset, batch_size=1, pin_memory=True, num_workers=cfg['num_workers'],
+                           drop_last=False)
 
     total_iters = len(trainloader_u) * cfg['epochs']
     previous_best = 0.0
@@ -89,14 +76,16 @@ def main():
         optimizer.load_state_dict(checkpoint['optimizer'])
         epoch = checkpoint['epoch']
         previous_best = checkpoint['previous_best']
-        
-        if rank == 0:
-            logger.info('************ Load from checkpoint at epoch %i\n' % epoch)
+        logger.info('************ Load from checkpoint at epoch %i\n' % epoch)
+
+    elif os.path.exists(cfg["pretrained_model_path"]):
+        checkpoint = torch.load(os.path.join(cfg["save_path"], 'latest.pth'))
+        model.load_state_dict(checkpoint['model'])
+        logger.info('************ Load from pretrained model')
     
     for epoch in range(epoch + 1, cfg['epochs']):
-        if rank == 0:
-            logger.info('===========> Epoch: {:}, LR: {:.5f}, Previous best: {:.2f}'.format(
-                epoch, optimizer.param_groups[0]['lr'], previous_best))
+        logger.info('===========> Epoch: {:}, LR: {:.5f}, Previous best: {:.2f}'.format(
+            epoch, optimizer.param_groups[0]['lr'], previous_best))
 
         total_loss = AverageMeter()
         total_loss_x = AverageMeter()
@@ -104,9 +93,9 @@ def main():
         total_loss_w_fp = AverageMeter()
         total_mask_ratio = AverageMeter()
 
-        trainloader_l.sampler.set_epoch(epoch)
-        trainloader_u.sampler.set_epoch(epoch)
-        trainloader_u_mix.sampler.set_epoch(epoch + cfg['epochs'])
+        # trainloader_l.sampler.set_epoch(epoch)
+        # trainloader_u.sampler.set_epoch(epoch)
+        # trainloader_u_mix.sampler.set_epoch(epoch + cfg['epochs'])
         
         loader = zip(trainloader_l, trainloader_u, trainloader_u_mix)
 
@@ -187,14 +176,13 @@ def main():
             lr = cfg['lr'] * (1 - iters / total_iters) ** 0.9
             optimizer.param_groups[0]["lr"] = lr
             
-            if rank == 0:
-                writer.add_scalar('train/loss_all', loss.item(), iters)
-                writer.add_scalar('train/loss_x', loss_x.item(), iters)
-                writer.add_scalar('train/loss_s', (loss_u_s1.item() + loss_u_s2.item()) / 2.0, iters)
-                writer.add_scalar('train/loss_w_fp', loss_u_w_fp.item(), iters)
-                writer.add_scalar('train/mask_ratio', mask_ratio, iters)
-            
-            if (i % (len(trainloader_u) // 8) == 0) and (rank == 0):
+            writer.add_scalar('train/loss_all', loss.item(), iters)
+            writer.add_scalar('train/loss_x', loss_x.item(), iters)
+            writer.add_scalar('train/loss_s', (loss_u_s1.item() + loss_u_s2.item()) / 2.0, iters)
+            writer.add_scalar('train/loss_w_fp', loss_u_w_fp.item(), iters)
+            writer.add_scalar('train/mask_ratio', mask_ratio, iters)
+        
+            if (i % (len(trainloader_u) // 8) == 0):
                 logger.info('Iters: {:}, Total loss: {:.3f}, Loss x: {:.3f}, Loss s: {:.3f}, Loss w_fp: {:.3f}, Mask ratio: '
                             '{:.3f}'.format(i, total_loss.avg, total_loss_x.avg, total_loss_s.avg, 
                                             total_loss_w_fp.avg, total_mask_ratio.avg))
@@ -224,28 +212,26 @@ def main():
         dice_class = [dice * 100.0 / len(valloader) for dice in dice_class]
         mean_dice = sum(dice_class) / len(dice_class)
         
-        if rank == 0:
-            for (cls_idx, dice) in enumerate(dice_class):
-                logger.info('***** Evaluation ***** >>>> Class [{:} {:}] Dice: '
-                            '{:.2f}'.format(cls_idx, CLASSES[cfg['dataset']][cls_idx], dice))
-            logger.info('***** Evaluation ***** >>>> MeanDice: {:.2f}\n'.format(mean_dice))
-            
-            writer.add_scalar('eval/MeanDice', mean_dice, epoch)
-            for i, dice in enumerate(dice_class):
-                writer.add_scalar('eval/%s_dice' % (CLASSES[cfg['dataset']][i]), dice, epoch)
+        for (cls_idx, dice) in enumerate(dice_class):
+            logger.info('***** Evaluation ***** >>>> Class [{:} {:}] Dice: '
+                        '{:.2f}'.format(cls_idx, CLASSES[cfg['dataset']][cls_idx], dice))
+        logger.info('***** Evaluation ***** >>>> MeanDice: {:.2f}\n'.format(mean_dice))
+        
+        writer.add_scalar('eval/MeanDice', mean_dice, epoch)
+        for i, dice in enumerate(dice_class):
+            writer.add_scalar('eval/%s_dice' % (CLASSES[cfg['dataset']][i]), dice, epoch)
 
         is_best = mean_dice > previous_best
         previous_best = max(mean_dice, previous_best)
-        if rank == 0:
-            checkpoint = {
-                'model': model.state_dict(),
-                'optimizer': optimizer.state_dict(),
-                'epoch': epoch,
-                'previous_best': previous_best,
-            }
-            torch.save(checkpoint, os.path.join(args.save_path, 'latest.pth'))
-            if is_best:
-                torch.save(checkpoint, os.path.join(args.save_path, 'best.pth'))
+        checkpoint = {
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'epoch': epoch,
+            'previous_best': previous_best,
+        }
+        torch.save(checkpoint, os.path.join(args.save_path, 'latest.pth'))
+        if is_best:
+            torch.save(checkpoint, os.path.join(args.save_path, 'best.pth'))
 
 
 if __name__ == '__main__':
